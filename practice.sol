3c1661e5fcc8424726450da2cbe8acc8bdd533b6
Jordan Jones <jordanjones0902@gmail.com>
2023-09-07 20:00:17 -0400

    Notes

--- practice.sol
+++ practice.sol
@@ -1,5 +1,14 @@
 SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18; 
-contract YoMammuy {
+contract Practice {
+/* 
+* Booleans are false by defualt 
+* uint and int are 256 by default
+*/ 
 
+uint256  favoriteNumber; // 0; by defualt, Nothing was assined
+
+function store(uint256 _favoriteNumber) public {
+  favoriteNumber = _facvoriteNumber;
+}
 }
