b7c2b238eb67aecf3f1b02be13264fbadd0ac840
Jordan Jones <jordanjones0902@gmail.com>
2023-09-09 20:03:34 -0400

    More pracrtice

--- practice.sol
+++ practice.sol
@@ -5,10 +5,8 @@ contract Practice {
 * Booleans are false by defualt 
 * uint and int are 256 by default
 */ 
-
-uint256  favoriteNumber; // 0; by defualt, Nothing was assined
-
-function store(uint256 _favoriteNumber) public {
-  favoriteNumber = _facvoriteNumber;
+uint public MydogJimbo;
+funtion Store(uint256 _MydogJimbo) public {
+MydogJimbo = _MydogJimbo;
 }
 }
