ee2a034336c367120208ef2215858cab83662665
Jordan Jones <jordanjones0902@gmail.com>
2023-09-26 09:26:38 -0400

    Mpre practice

--- practice.sol
+++ practice.sol
@@ -4,9 +4,23 @@ contract Practice {
 /* 
 * Booleans are false by defualt 
 * uint and int are 256 by default
-*/ 
-uint public MydogJimbo;
+* Your data is always visble on the blockchain, 
+ aking something public allows other smart contracts 
+ to call it
+  * only int can be negative*/ 
+uint MydogJimbo;
 funtion Store(uint256 _MydogJimbo) public {
-MydogJimbo = _MydogJimbo;
+MydogJimbo = _MydogJimbo + 1;
 }
+// Arrays start from 0,1,2,3,4,etc
+//uint256 [] ListOfNumbers;
+struct person {
+  uint256 Bigdogs;
+  string name;
+  }
+  persom public mydog = person({Bigdogs: 1, name: "Jimbo"});
+
+str
+ function retreive() publiv view returns(uint256) {
+  return MydogJimbo
 }
